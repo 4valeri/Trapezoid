a298731ffeff20849cc4b13d8c1861858bded2f3
4valeri <4valeri@mail.bg>
2016-01-16 17:45:53 +0000

    test
    test

---
+++ Trapezoid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Trapezoid
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+
+        double area = (((a + b) / 2) * h);
+
+        Console.WriteLine("{0:f2}", area);
+
+    }
+}
+
